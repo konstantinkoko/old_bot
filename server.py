f988a9f405b0fd177a31bb4855247ac001f3d788
Константин <kolobokkoko@gmail.com>
2021-07-06 21:00:22 +0500

    project

--- server.py
+++ server.py
@@ -4,15 +4,22 @@ from . import config
 
 app = Flask(__name__)
 
-api_url = config.api_url
-telegram_token = config.telegram_token
-server_url = config.server_url
+api_url = config.API_URL
+telegram_token = config.TELEGRAM_TOKEN
+server_url = config.SERVER_URL
 
 @app.route("/", methods=["POST"])
 def process():
     print(request.json)
+
+'''
 def hello():
     return "Hello World!"
+'''
 
 if __name__ == "__main__":
+
+    # server_url update (ngrok)
+    requests.post('https://api.telegram.org/bot' + telegram_token + '/setWebhook', data={'url': server_url})
+
     app.run()

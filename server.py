151f602ff26f2d49c436b1bb97a7f78f4898e12e
Константин <kolobokkoko@gmail.com>
2021-07-07 17:15:13 +0500

    telegram bot

--- server.py
+++ server.py
@@ -1,25 +1,30 @@
 from flask import Flask, request
 import requests
-from . import config
+import config
 
 app = Flask(__name__)
 
-api_url = config.API_URL
-telegram_token = config.TELEGRAM_TOKEN
-server_url = config.SERVER_URL
+def get_api_url(method):
+    api_url = f"https://api.telegram.org/bot{config.TELEGRAM_TOKEN}/{method}"
+    return api_url
+
+def send_message(chat_id, text):
+    method = "sendMessage"
+    url = get_api_url(method)
+    data = {"chat_id": chat_id, "text": text}
+    requests.post(url, data=data)
+
 
 @app.route("/", methods=["POST"])
 def process():
+    chat_id = request.json["message"]["chat"]["id"]
     print(request.json)
-
-'''
-def hello():
-    return "Hello World!"
-'''
+    send_message(chat_id, "Привет, Бубка! Налей Коте чаю, позязя!!!!!")
+    return {"ok": True}
 
 if __name__ == "__main__":
 
     # server_url update (ngrok)
-    requests.post('https://api.telegram.org/bot' + telegram_token + '/setWebhook', data={'url': server_url})
+    #requests.post(get_api_url("setWebhook"), data={'url': config.SERVER_URL})
 
     app.run()
